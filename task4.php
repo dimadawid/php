5b7310606475574aa70656b3c84a7c407c88fdf8
dimadawid <dimadawid@mail.ru>
2022-12-08 17:01:42 +0300

    dz

--- task4.php
+++ task4.php
@@ -1,9 +1,5 @@
 <?php
 
-$pattern = "/[0-9]{4}.[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2}/";
-
-$test = '2022/12/05 19:00';
-
 $period = new DatePeriod(
     new DateTime('05/12/1000 19:00'),
     new DateInterval('P1Y'),
